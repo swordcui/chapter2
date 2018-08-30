e1c3f1a9d4cb727fbfddf042a035b965e0b52371
cuicui <515702512@qq.com>
2018-08-30 22:46:03 +0800

    chapter2

--- src/main/java/org/smart4j/chapter2/controller/CustomerServlet.java
+++ src/main/java/org/smart4j/chapter2/controller/CustomerServlet.java
@@ -1,6 +1,37 @@
 package org.smart4j.chapter2.controller;
 
 
-public class CustomerServlet {
+import org.smart4j.chapter2.model.Customer;
+import org.smart4j.chapter2.service.CustomerService;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * 进入客户列表界面
+ */
+@WebServlet("/customer")
+
+public class CustomerServlet  extends HttpServlet {
+
+
+    private CustomerService customerService;
+
+
+    public void init() throws ServletException {
+        customerService = new CustomerService();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+        List<Customer> customerList = customerService.getCustomerList();
+        req.setAttribute("customerList",customerList);
+        req.getRequestDispatcher("/WEB-INF/view/customer.jsp").forward(req,resp);
+    }
 }
